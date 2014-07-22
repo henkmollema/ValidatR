d62c9d4c9ef1878da8280921a6bc2c1bc4fa5643
henkmollema <henkmollema@gmail.com>
2014-07-22 22:43:41 +0200

    Initial commit.

---
+++ Validatr/Configuration/ValidatrConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace Validatr.Configuration
+{
+    /// <summary>
+    /// Provides configugration for Validatr.
+    /// </summary>
+    public static class ValidatrConfig
+    {
+        /// <summary>
+        /// The serialization settings for JSON.NET, this field can be modified or replaced.
+        /// </summary>
+        public static JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+
+        /// <summary>
+        /// The HTTP status code of the output that is returned to the client. Default value is 'Bad Request' (400).
+        /// </summary>
+        public static HttpStatusCode HttpStatusCode = HttpStatusCode.BadRequest;
+
+        /// <summary>
+        /// The content type of the response containing the serialized model state. Default value is 'application/json'.
+        /// </summary>
+        public static string ContentType = "application/json";
+    }
+}
---
+++ Validatr/Filters/ValidatrActionFilterAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Validatr.Configuration;
+
+namespace Validatr.Filters
+{
+    /// <summary>
+    /// Represents an action filter that serialzes the model state of the current context and sends it in response to the client.
+    /// </summary>
+    public class ValidatrActionFilterAttribute : ActionFilterAttribute
+    {
+        /// <inheritdoc />
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.Controller.ViewData.ModelState.IsValid)
+            {
+                return;
+            }
+
+            var serializedModelState = JsonConvert.SerializeObject(
+                filterContext.Controller.ViewData.ModelState,
+                ValidatrConfig.JsonSerializerSettings);
+
+            var result = new ContentResult
+                             {
+                                 Content = serializedModelState,
+                                 ContentType = ValidatrConfig.ContentType
+                             };
+
+            filterContext.HttpContext.Response.StatusCode = (int)ValidatrConfig.HttpStatusCode;
+            filterContext.Result = result;
+        }
+    }
+}
